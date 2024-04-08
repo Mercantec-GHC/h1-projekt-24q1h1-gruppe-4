744b2ada312a9946f63d01d7e0f615af4cd55fce
AssisDk <50442679+AssisDk@users.noreply.github.com>
2024-04-08 05:52:24 +0200

    update/delete

--- BlazorApp/Services/DatabaseService.cs
+++ BlazorApp/Services/DatabaseService.cs
@@ -26,7 +26,7 @@ namespace BlazorApp.Services
                 connection.Open();
                 using (var command = new NpgsqlCommand(sql, connection))
                 {
-                    using (var reader = command.ExecuteReader())         
+                    using (var reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
@@ -127,7 +127,7 @@ namespace BlazorApp.Services
                                 Description = reader["Description"].ToString(),
                                 //Stars = reader.IsDBNull(reader.GetOrdinal("Stars")) ? 0.0f : Convert.ToSingle(reader["Stars"]),
                                 Type = reader["Type"].ToString(),
-                                Reviews = reader["Reviews"] == DBNull.Value ? new List<string>() : ((string[])reader["Reviews"]).ToList(),
+                                //Reviews = reader["Reviews"] == DBNull.Value ? new List<string>() : ((string[])reader["Reviews"]).ToList(),
                             };
                         }
                     }
@@ -160,12 +160,143 @@ namespace BlazorApp.Services
                     cmd.ExecuteNonQuery();
                 }
             }
-   
+
         }
+
+        public async Task<bool> ValidateUserAsync(int userId, string password)
+        {
+            string sql = "SELECT password FROM Users WHERE userid = @userId";
+
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+
+                using (var command = new NpgsqlCommand(sql, connection))
+                {
+
+                    command.Parameters.AddWithValue("@userId", userId);
+
+
+                    var result = await command.ExecuteScalarAsync();
+
+
+                    if (result != null)
+                    {
+
+                        if (result.ToString() == password)
+                        {
+                            return true; // Passwords match
+                        }
+                    }
+                }
+            }
+
+            return false; // No matching user or password
+        }
+
+
+        public List<Book> GetAllUserBooks(int UserID)
+        {
+            List<Book> userBooks = new List<Book>();
+
+
+            string sql = "SELECT * FROM Books WHERE UserId = @UserId";
+
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new NpgsqlCommand(sql, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+
+                            Book userBook = new Book
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Title = reader["Title"].ToString(),
+                                Author = reader["Author"].ToString(),
+                                Condition = reader["Condition"].ToString(),
+                                Category = reader["Category"].ToString(),
+                                Price = reader.IsDBNull(reader.GetOrdinal("Price")) ? 0.0m : Convert.ToDecimal(reader["Price"]),
+                                ImagePath = reader["ImagePath"].ToString(),
+                                Language = reader["Language"].ToString(),
+                                ReleaseDate = reader.GetDateTime(reader.GetOrdinal("ReleaseDate")),
+                                Format = reader["Format"].ToString(),
+                                ISBN = reader.IsDBNull(reader.GetOrdinal("ISBN")) ? 0L : Convert.ToInt64(reader["ISBN"]),
+                                Weight = reader.IsDBNull(reader.GetOrdinal("Weight")) ? 0.0f : Convert.ToSingle(reader["Weight"]),
+                                Pages = reader.IsDBNull(reader.GetOrdinal("Pages")) ? 0 : Convert.ToInt32(reader["Pages"]),
+                                Description = reader["Description"].ToString(),
+                                //UserId = Convert.ToInt32(reader["userid"])
+                                // Add more properties as needed
+                            };
+
+                            // Add the used book to the list
+                            userBooks.Add(userBook);
+                        }
+                    }
+                }
+            }
+
+            return userBooks;
+        }
+
+        public async Task UpdateBookAsync(Book itemToUpdate)
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var cmd = new NpgsqlCommand(@"UPDATE Books
+                                                        SET Title = @Title, Author = @Author, Condition = @Condition, Category = @Category, Price = @Price, 
+                                                        ImagePath = @ImagePath, Language = @Language, ReleaseDate = @ReleaseDate, ISBN = @ISBN, Pages = @Pages, 
+                                                        Description = @Description
+                                                        WHERE Id = @Id", connection))
+                {
+                    cmd.Parameters.AddWithValue("@Title", itemToUpdate.Title);
+                    cmd.Parameters.AddWithValue("@Author", itemToUpdate.Author);
+                    cmd.Parameters.AddWithValue("@Condition", itemToUpdate.Condition);
+                    cmd.Parameters.AddWithValue("@Category", itemToUpdate.Category);
+                    cmd.Parameters.AddWithValue("@Price", itemToUpdate.Price);
+                    cmd.Parameters.AddWithValue("@ImagePath", itemToUpdate.ImagePath);
+                    cmd.Parameters.AddWithValue("@Language", itemToUpdate.Language);
+                    cmd.Parameters.AddWithValue("@ReleaseDate", itemToUpdate.ReleaseDate);
+                    cmd.Parameters.AddWithValue("@ISBN", itemToUpdate.ISBN);
+                    cmd.Parameters.AddWithValue("@Pages", itemToUpdate.Pages);
+                    cmd.Parameters.AddWithValue("@Description", itemToUpdate.Description);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+        }
+
+        public async Task DeleteBookAsync(int bookId)
+        {
+
+            using(var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var cmd = new NpgsqlCommand(@"DELETE FROM Books WHERE Id = @Id", connection))
+                {
+                    cmd.Parameters.AddWithValue("@Id", bookId); ;
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+        }
+
+
+
     }
-
-
 }
+
+
+
 
 
 
